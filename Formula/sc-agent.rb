f6ed051863843bbc3c7ea01c469fef7ad1c343fe
Tudor TIMCU <tudor@aikido.dev>
2025-12-09 15:56:39 +0200

    updates

--- Formula/sc-agent.rb
+++ Formula/sc-agent.rb
@@ -40,8 +40,9 @@ class ScAgent < Formula
     run [opt_bin/"sc-agent"]
     run_at_load true
     keep_alive true
-    log_path var/"log/sc-agent.log"
-    error_log_path var/"log/sc-agent.error.log"
+    require_root true
+    log_path "/var/log/aikidosecurity/sc-agent.log"
+    error_log_path "/var/log/aikidosecurity/sc-agent.error.log"
   end
 
   test do
